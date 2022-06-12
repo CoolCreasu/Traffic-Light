01fbf6573d85cdd05f8fc6fe4106fc20c1f1f9a3
CoolCreasu <svenboeckx2@gmail.com>
2022-06-12 19:39:14 +0200

    Update Traffic-Light.ino

--- Traffic-Light/Traffic-Light.ino
+++ Traffic-Light/Traffic-Light.ino
@@ -1,17 +1,59 @@
 const int button = 5;
 
+enum states
+{
+  standard,
+  maintenance,
+};
+
+int state;
+
 void setup()
 {
   pinMode(button, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(button), changeMode, FALLING);
+  attachInterrupt(digitalPinToInterrupt(button), changeState, FALLING);
+
+  state = standard;
 }
 
 void loop()
 {
+  switch (state)
+  {
+    case standard:
+    standardState();
+    break;
+    case maintenance:
+    maintenanceState();
+    break;
+    default:
+    state = maintenance;
+    break;
+  }
+}
+
+void standardState()
+{
   
 }
 
-void changeMode()
+void maintenanceState()
 {
   
+}
+
+void changeState()
+{
+  switch(state)
+  {
+    case standard:
+    state = maintenance;
+    break;
+    case maintenance:
+    state = standard;
+    break;
+    default:
+    state = maintenance;
+    break;
+  }
 }
